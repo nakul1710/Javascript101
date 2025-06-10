670ed416ce9e74874a40b23cede8a86c0163cd72
Nakul Yadav <nakulyadav@Nakuls-MacBook-Pro.local>
2025-06-10 10:50:45 +0530

    JAVASCRIPT101-1st lecture

---
+++ IstJavascript.js
@@ -0,0 +1,86 @@
+// console.log("Hello Javascript");
+// console.log("Nakul Yadav");
+// console.log("We are learning javascript");
+
+
+//variable in javascript
+
+// FullName = "Nakul Yadav";
+// console.log(FullName);
+// age = 19;
+// console.log(age);
+// x = null;
+// y = undefined;
+// console.log(x,y);
+
+// $name = "Nakul";
+// console.log($name);
+// _name = "Yadav";
+// console.log(_name);
+
+
+//let,var and const
+
+//var can be redeclared and updated
+//let not be redeclared but can be updated
+//cosnt can be redeclared and can't be updated
+
+
+//objects Key: value pairs
+
+// let Student = {
+//     name: "Nakul Yadav",
+//     age: 19,
+//     cgpa : 8.2,
+//     isPass : true,
+
+// }
+// console.log(Student);
+// console.log(Student.name);
+// console.log(Student.cgpa);
+// console.log(Student.age);
+
+
+// Student["name"] = "Mohit Sharma";
+// console.log(Student["name"]);
+
+
+
+//practice Q1
+
+
+const product = {
+    name: "Parker Jotter",
+    rating: 4,
+    price: 300,
+    offer: 5
+};
+
+const profile ={
+    username :  "@nakulyadav1710",
+    ifFollow: false,
+    following : 330,
+    follower : 200,
+    Bio : "@Google|@Ex-microsoft|@IIT-D|@Ex-meta"
+}
+
+console.log(profile.Bio);
+console.log(profile.username);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
