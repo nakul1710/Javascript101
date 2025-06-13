3a5af9d36c8a4b89e5d1e5b6b71510a9c10f0f67
Nakul Yadav <nakulyadav@Nakuls-MacBook-Pro.local>
2025-06-13 11:10:48 +0530

    JAVASCRIPT101-3rd lecture

--- index2.js
+++ index2.js
@@ -146,22 +146,21 @@ hello world in our consolewindow.*/
 // }
 
 
-let score = prompt("Enter number between 0 - 100:");
-let grade;
+// let score = prompt("Enter number between 0 - 100:");
+// let grade;
 
-if(score>= 80 && score<=100){
-    grade = "A";
-}else if(score >=70 && score<=79){
-    grade = "B";
-}else if(score>=60 && score<=69){
-    grade = "C";
-}else if(score>50 && score<=59){
-    grade = "D";
-}else{
-    grade = "F";
-}
-
-console.log("your grade is",grade);
+// if(score>= 80 && score<=100){
+//     grade = "A";
+// }else if(score >=70 && score<=79){
+//     grade = "B";
+// }else if(score>=60 && score<=69){
+//     grade = "C";
+// }else if(score>50 && score<=59){
+//     grade = "D";
+// }else{
+//     grade = "F";
+// }
+// console.log("your grade is",grade);
 
 
 
---
+++ index3.js
@@ -0,0 +1,41 @@
+//loops: piece of code executed again & again
+
+// for(let i=1; i <= 5; i++){
+// console.log("Apna College");
+// }
+
+// console.log("Loop has ended")
+
+
+//calculate 1 to 5
+
+// let sum = 0;
+// for(let i=1; i<=5; i++){
+//     sum = sum + i;
+// }
+// console.log("sum = ",sum);
+
+
+//loops in js
+//infinite loops
+
+// for(i=1; i>=0; i++ ){
+//    console.log("Hello Javascript");
+// }
+
+
+// let sum = 0;
+// let n = 10;
+// for(i=1; i<=n; i++){
+//     sum = sum + i;
+// }
+// console.log("sum =",sum);
+
+
+
+//while loops
+
+let i = 0;
+while(i<=5){
+  console.log("i =",i);
+}
